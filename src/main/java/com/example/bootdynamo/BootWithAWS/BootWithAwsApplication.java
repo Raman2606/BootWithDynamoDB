3a2fdf394908d5fc8b0daa7de629f98507169315
Raman Garg <Raman.Garg@gfs.com>
2019-12-08 00:30:33 +0530

    integrated and validations added

---
+++ src/main/java/com/example/bootdynamo/BootWithAWS/BootWithAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootdynamo.BootWithAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWithAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootWithAwsApplication.class, args);
+	}
+
+}
